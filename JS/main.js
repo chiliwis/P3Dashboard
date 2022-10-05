89743587ba608c02d3dbf3d612f754ee8ea36620
chiliwis <cristinasanchezg@gmail.com>
2022-10-05 18:29:25 -0500

    Tabla con datos y grafica base

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ console.log("Entro a main.js")
 
 const base_url = "http://ucamp.alumnos.dev4humans.com.mx/Main/endpoint_ingresos_mensuales";
 const tblIngresos = document.getElementById("tblIngresos");
-
+const grafica = document.getElementById('myChart').getContext('2d');
 
 function cargarIngresos() {
     fetch(base_url,
@@ -14,23 +14,60 @@ function cargarIngresos() {
         .then(result => {
             console.log(result);
             tblIngresos.innerHTML = "";
-        
-    for (const ingresos of result.data) {
-        let tr = `<tr>
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
+
+            for (const ingresos of result.data) {
+                let tr = `<tr>
         <td>${ingresos.nombre}</td>
         <td>${ingresos.id}</td>
         <td>${ingresos.monto}</td>
 
         </tr>`;
-        tblIngresos.innerHTML+=tr;
-    }
-    if(result.data.length == 0){
-        tblIngresos.innerHTML = `<tr><td colspan="5" class="text-center">No hay ingresos</td></tr>`;
-    }
-    })
-    .catch(error=> {
-        console.log("Error");
-    })
+                tblIngresos.innerHTML += tr;
+            }
+            if (result.data.length == 0) {
+                tblIngresos.innerHTML = `<tr><td colspan="5" class="text-center">No hay ingresos</td></tr>`;
+            }
+        })
+        .catch(error => {
+            console.log("Error");
+        })
 }
 
 cargarIngresos();
