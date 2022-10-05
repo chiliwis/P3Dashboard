cfda1e48173f7d5a6b77742deaa5a7e90ecdc6a6
chiliwis <cristinasanchezg@gmail.com>
2022-10-05 18:06:43 -0500

    Tabla jala info

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,36 @@
 console.log("Entro a main.js")
+
+const base_url = "http://ucamp.alumnos.dev4humans.com.mx/Main/endpoint_ingresos_mensuales";
+const tblIngresos = document.getElementById("tblIngresos");
+
+
+function cargarIngresos() {
+    fetch(base_url,
+        {
+            method: "GET"
+        }
+    )
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            tblIngresos.innerHTML = "";
+        
+    for (const ingresos of result.data) {
+        let tr = `<tr>
+        <td>${ingresos.nombre}</td>
+        <td>${ingresos.id}</td>
+        <td>${ingresos.monto}</td>
+
+        </tr>`;
+        tblIngresos.innerHTML+=tr;
+    }
+    if(result.data.length == 0){
+        tblIngresos.innerHTML = `<tr><td colspan="5" class="text-center">No hay ingresos</td></tr>`;
+    }
+    // })
+    // .catch(error=> {
+    //     console.log("Error");
+    })
+}
+
+cargarIngresos();
